531d1c358f91d1df83ccae4a89c65dc5e028d15b
Pablo Pirata <pablo@studocu.com>
2025-08-10 00:42:22 +0200

    add total costs

---
+++ app/code/Werules/Inventory/Block/Adminhtml/Category/Overview.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Werules\Inventory\Block\Adminhtml\Category;
+
+use Magento\Backend\Block\Template;
+use Magento\Backend\Block\Template\Context;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\CatalogInventory\Api\StockStateInterface;
+
+class Overview extends Template
+{
+    protected $_categoryCollectionFactory;
+    protected $_productCollectionFactory;
+    protected $_stockState;
+
+    public function __construct(
+        Context $context,
+        CategoryCollectionFactory $categoryCollectionFactory,
+        ProductCollectionFactory $productCollectionFactory,
+        StockStateInterface $stockState,
+        array $data = []
+    ) {
+        $this->_categoryCollectionFactory = $categoryCollectionFactory;
+        $this->_productCollectionFactory = $productCollectionFactory;
+        $this->_stockState = $stockState;
+        parent::__construct($context, $data);
+    }
+
+    public function getCategoryInventoryCosts()
+    {
+        $categoryCosts = [];
+        $categoryCollection = $this->_categoryCollectionFactory->create()
+            ->addAttributeToSelect('name')
+            ->addAttributeToFilter('level', 2); // Main categories
+
+        foreach ($categoryCollection as $category) {
+            $totalCost = 0;
+            $productCollection = $this->_productCollectionFactory->create();
+            $productCollection->addCategoryFilter($category)
+                ->addAttributeToSelect(['cost']);
+
+            foreach ($productCollection as $product) {
+                $stockQty = $this->_stockState->getStockQty($product->getId(), $product->getStore()->getWebsiteId());
+                if ($stockQty > 0 && $product->getCost()) {
+                    $totalCost += $product->getCost() * $stockQty;
+                }
+            }
+
+            if ($totalCost > 0) {
+                $categoryCosts[] = [
+                    'name' => $category->getName(),
+                    'cost' => $totalCost
+                ];
+            }
+        }
+
+        return $categoryCosts;
+    }
+}
---
+++ app/code/Werules/Inventory/Controller/Adminhtml/Category/Overview.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Werules\Inventory\Controller\Adminhtml\Category;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Overview extends Action
+{
+    const ADMIN_RESOURCE = 'Werules_Inventory::category_overview';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Category Overview'));
+        return $resultPage;
+    }
+}
